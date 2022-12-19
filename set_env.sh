e80f77215e3266d05f29de6e312b10fe7a52b977
atefdev <atef.programmer@gmail.com>
2022-12-19 22:20:22 +0200

    my final project

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Amm112233
+export POSTGRES_HOST=db-inst.csvhjbdyvdda.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-714657236685-dev
+#export AWS_BUCKET=arn:aws:s3:::final-026069378683-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
